c861a1926341b2a451844f5be70ea7e52b406752
Steve Mitchell <smitchell@bandwidth.com>
2015-01-20 15:50:21 -0700

    check for null in Utils.response2JSONArray method

--- src/main/java/com/bandwidth/sdk/Utils.java
+++ src/main/java/com/bandwidth/sdk/Utils.java
@@ -9,7 +9,7 @@ import org.json.simple.parser.JSONParser;
 public class Utils {
 
 	public static JSONArray response2JSONArray(RestResponse response) throws IOException {
-		if (response.isJson() && response.getResponseText() != null) {
+		if (response != null && response.isJson() && response.getResponseText() != null) {
 						
 			// TODO this will throw an exception if there is only one object in the JSON.
 			// need to refactor to generate an array of one if there is a single object.
