e9b36c266d2ed1dcbd2cab84494fc2b7830ee5a5
Lucas Nunes <lnunes@daitangroup.com>
2015-06-10 14:13:37 -0300

    Fixed exception message

--- src/main/java/com/bandwidth/sdk/exception/MissingCredentialsException.java
+++ src/main/java/com/bandwidth/sdk/exception/MissingCredentialsException.java
@@ -9,14 +9,14 @@ public class MissingCredentialsException extends IOException {
     public MissingCredentialsException() {
         super("Missing credentials. \n" +
                 "\tThere are 3 ways to set these credentials:\n\n" +
-                "\t1. Via environment variables:\n" +
-                "\t\texport BANDWIDTH_USER_ID=<myUserId>\n" +
-                "\t\texport BANDWIDTH_API_TOKEN=<myApiToken>\n" +
-                "\t\texport BANDWIDTH_API_SECRET=<myApiSecret>\n" +
-                "\t2. Via Java VM System Properties, set as -D arguments on the VM command line:\n" +
+                "\t1. Via Java VM System Properties, set as -D arguments on the VM command line:\n" +
                 "\t\t-Dcom.bandwidth.userId=<myUserId>\n" +
                 "\t\t-Dcom.bandwidth.apiToken=<myApiToken>\n" +
                 "\t\t-Dcom.bandwidth.apiSecret=<myApiSecret>\n" +
+                "\t2. Via environment variables:\n" +
+                "\t\texport BANDWIDTH_USER_ID=<myUserId>\n" +
+                "\t\texport BANDWIDTH_API_TOKEN=<myApiToken>\n" +
+                "\t\texport BANDWIDTH_API_SECRET=<myApiSecret>\n" +
                 "\t3. Directly by way of a method call on the BandwidthClient object\n" +
                 "\t\tBandwidthClient.getInstance().setCredentials(<myUserId>, <myApiToken>, <myApiSecret>)\n\n" +
                 "\tNotice: if credentials are not set explicitly, the sdk will first look for VM properties. " +
