4bad0a1fba53c7d5cba939a2ec001c6d4248aa13
Daniel Tolbert <danielptolbert@gmail.com>
2016-10-28 10:17:53 -0400

    Update TextMessageExample.java

--- src/main/java/com/bandwidth/sdk/examples/TextMessageExample.java
+++ src/main/java/com/bandwidth/sdk/examples/TextMessageExample.java
@@ -13,27 +13,27 @@ public class TextMessageExample {
 	 * @param args the args.
 	 */
 	public static void main(final String[] args) {
-        // There are two ways to set your creds, e.g. your App Platform userId, api token and api secret
+		// There are two ways to set your creds, e.g. your App Platform userId, api token and api secret
 		// you can set these as environment variables or set them with the 
 		// BandwidthClient.getInstance(userId, apiToken, apiSecret) method.
-		// 
+		//
 		// Use the setenv.sh script to set the env variables
-    	// BANDWIDTH_USER_ID
-    	// BANDWIDTH_API_TOKEN
-    	// BANDWIDTH_API_SECRET
-		// 
+		// BANDWIDTH_USER_ID
+		// BANDWIDTH_API_TOKEN
+		// BANDWIDTH_API_SECRET
+		//
 		// or uncomment this line and set them here
 		// BandwidthClient.getInstance(userId, apiToken, apiSecret);
-		
+
 		// put your numbers in here
 		final String toNumber = "+1";// your phone number here
 		final String fromNumber = "+1";// this is a number that is allocated on the AppPlatform. You can do this
 							 // via the dev console or with the SDK (see AllocateNumberExample)
-		
+
 		try {
 
-            final Message message = Message.create(toNumber, fromNumber, "Test, test! What up from App Platform");
-			
+			final Message message = Message.create(toNumber, fromNumber, "Test, test! What up from App Platform");
+
 			System.out.println("message:" + message);
 		}
 		catch(final Exception e) {
